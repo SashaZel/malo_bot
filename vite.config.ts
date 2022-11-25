beb761321815784eb96d83819c7cfb36bcd6aeb4
Alexander Zelenkov <lll555@yandex.ru>
2022-11-25 16:31:52 +0700

    fix(vite-config): set base for ./malo_bot/ root

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-//https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/"
-// })
-
-// export default defineConfig(({ command }) => {
-//   if (command === "serve"){
-//     return {
-//     plugins: [react()],
-//     base: "/",
-//   };
-//   }
-//   if (command === "build") {
-//     return {
-//       plugins: [react()],
-//       base: "",
-//     };
-//   }
-  
-// });
-
 export default defineConfig({
   plugins: [react()],
   base: "/malo_bot/"
